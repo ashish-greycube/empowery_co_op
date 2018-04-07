f9a1054ce123afb2b8d720b927ccacc3ce0ecb15
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-07 18:49:15 +0530

    role name in title case

---
+++ empowery_co_op/config/CO-OP.py
@@ -0,0 +1,18 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Empowery CO-OP"),
+			"items": [
+				{
+					"type": "report",
+					"name": "Linked Customer",
+				}
+				{
+					"type": "report",
+					"name": "Linked Supplier",
+				}
+			]
+		}
+    ]
