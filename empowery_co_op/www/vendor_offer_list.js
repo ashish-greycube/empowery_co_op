8e485554de75250518c76da65fd72e43bde258f6
ashish-greycube <mr.ashish.shah@gmail.com>
2018-09-16 11:15:50 +0530

    better code

---
+++ empowery_co_op/www/vendor_offer_list.js
@@ -0,0 +1,106 @@
+
+
+$(document).ready(function() {
+
+    if (frappe.session.user!='Guest') {
+        $('[name="sender_name"]').val($('.full-name').text());
+    }
+
+    $('select').change(function(){
+        var location='['+$('#location_category_selector').val()+']'
+        var category='['+$('#service_category_selector').val()+']'
+      //hide all first
+      $('.supplier').hide()
+      //show the matching ones
+      $('.supplier[data-category*="' + category + '"][data-location*="'+location+'"]').show()
+    
+    });
+
+    // button for sign-up event
+  
+    $('.btn-request').click(function() {
+      // hide any message
+      $("#form-alert").toggle(false);
+
+        //selected vendor
+        var sel = $('input[type=checkbox]:checked').map(function(_, el) {
+            return $(el).val();
+        }).get();
+    
+        if(sel.length == 0) {
+            msgprint('Please select supplier for inquiry');
+            return;
+        }  
+  
+      var args = {
+          sender_name: $('[name="sender_name"]').val(),
+          company_name: $('[name="company_name"]').val(),
+          email: $('[name="email"]').val(),
+          phone: $('[name="phone"]').val()
+        }
+  
+      // all mandatory
+      if(!(args.sender_name && args.company_name && args.email
+          && args.phone)) {
+         msgprint("All fields are necessary. Please try again.");
+         return;
+      }
+  
+      // email is valid
+      if(!valid_email(args.email)) {
+          msgprint('Please enter a valid email id');
+          return;
+      }
+
+
+
+    // check if member or guest
+    var is_guest = false
+    if (getCookie("full_name")=="Guest"){
+        is_guest = true
+    }
+      // compose the message
+      var message = "Company: " + args.company_name + "\n"
+          + "Name: " + args.sender_name + "\n"
+          + "Email: " + args.email + "\n"
+          + "Phone: " + args.phone + "\n" 
+          + "Is Guest? : " + is_guest + "\n"
+
+
+    frappe.call({
+            method:'empowery_co_op.www.supplier_list.send_email',
+            args:{
+                name:args.sender_name,
+                company:args.company_name,
+                email:args.email,
+                phone:args.phone,
+                is_guest: is_guest,
+                vendor_list:sel
+            },
+            callback: function(r) {
+                          if(r.message) {
+                              data=r.message
+                              msgprint('Email sent to '+data+ ' vendor');
+                          }}});
+ 
+    var message = message + "Inquiry for vendor: "+sel
+      erpnext.send_message({
+        subject:'Inquiry for vendor :'+sel,
+        sender: $('[name="email"]').val(),
+        status: 'Open',
+        message: message,
+        callback: function(r) {
+              // print return message
+          msgprint(r.message);
+  
+              // clear all inputs
+          $(':input').val('');
+        }
+      });
+    });
+  
+  });
+  
+  var msgprint = function(txt) {
+      $("#form-alert").html(txt).toggle(true);
+  }
---
+++ empowery_co_op/www/vendor_offer_list.py
@@ -0,0 +1,114 @@
+from __future__ import unicode_literals
+import frappe, json
+from frappe.utils import split_emails
+from frappe import _
+import email
+from frappe.modules import get_module_path, scrub
+
+def get_context(context):
+    context.no_cache = 1
+    # setup vendor slideshow
+    vendor_slideshow_doc = frappe.get_single('Vendor Carousel and Email template')
+    if vendor_slideshow_doc.slideshow:
+        slideshow = frappe.get_doc("Website Slideshow", vendor_slideshow_doc.slideshow)
+        context.slideshow=vendor_slideshow_doc.slideshow
+        context.update({
+            "slides": slideshow.get({"doctype":"Website Slideshow Item"}),
+            "slideshow_header": slideshow.header or ""
+        })
+
+# setup selection dropdown for vendor category
+    vendor_categories=frappe.db.sql("""select distinct(service_category) from `tabSupplier` where service_category is not null""",as_dict=1)
+    context.vendor_category_dropdown=vendor_categories
+
+# setup selection dropdown for vendor location
+    vendor_locations=frappe.db.sql("""select distinct(location.geo_location) as location_category from `tabSupplier Geo Location Detail` location
+inner join `tabSupplier` supplier on location.parent=supplier.name
+where location.parentfield='geo_location' and supplier.display_on_partner_listing_page=1
+order by supplier.name,location.geo_location""",as_dict=1)
+    context.vendor_location_dropdown=vendor_locations
+
+# setup vendor geo location list
+    vendor_location=frappe.db.sql("""select supplier.name,supplier.service_category,location.geo_location as location_category 
+from `tabSupplier` supplier 
+LEFT OUTER JOIN `tabSupplier Geo Location Detail` location
+on location.parent=supplier.name
+where supplier.display_on_partner_listing_page=1
+order by supplier.name,location.geo_location""",as_dict=1)
+   
+ 
+    suppliers = {}
+    for d in vendor_location:
+        if not suppliers.get(d['name'],None):
+            suppliers[d['name']] = []
+            suppliers[d['name']].append('[all_location]')
+        if d['location_category']:
+            suppliers[d['name']].append("["+d['location_category']+"]")
+  
+    for key in suppliers:
+        suppliers[key] = " ".join(set(suppliers[key]))
+
+    context.location_category=suppliers
+
+    #setup form fill up data
+    if frappe.session.user!='Guest':
+        context.form_data=frappe.db.sql("""select customer.link_name as company,IF(contact.phone IS NULL or contact.phone = '', contact.mobile_no, contact.phone) as phone
+            from `tabContact` contact
+            inner join `tabDynamic Link` customer on
+            customer.parent=contact.name
+            where customer.link_doctype='Customer' and customer.parenttype='Contact'
+            and contact.user=%s""",frappe.session.user,as_dict=1)
+        if len(context.form_data)!=0:
+            context.form_data=context.form_data[0]
+        else:
+            context.form_data=0
+
+    # setup vedor data
+    if frappe.session.user=='Guest':
+        context.vendor_supplier = frappe.db.sql("""select name,supplier_type,image,service_category,offer_headline,offer_expiration_date,offer_summary,'guest' as more_details,geo_eligibility
+        from `tabSupplier`
+        where display_on_partner_listing_page=1 and
+        supplier_type ='Supplier Partner' order by name""",as_dict=1)
+
+        context.vendor_affiliate = frappe.db.sql("""select name,supplier_type,image,service_category,offer_headline,offer_expiration_date,offer_summary,'guest' as more_details,geo_eligibility
+        from `tabSupplier`
+        where display_on_partner_listing_page=1 and
+        supplier_type ='Affiliate Partner' order by name""",as_dict=1)
+
+    else:
+        context.vendor_supplier = frappe.db.sql("""select name,supplier_type,image,service_category,offer_headline,offer_expiration_date,offer_summary,more_details as more_details ,geo_eligibility,disclaimer_details,contact_email_for_offers
+        from `tabSupplier`
+        where display_on_partner_listing_page=1 and
+        supplier_type ='Supplier Partner' order by name""",as_dict=1)
+
+        context.vendor_affiliate = frappe.db.sql("""select name,supplier_type,image,service_category,offer_headline,offer_expiration_date,offer_summary,more_details as more_details,geo_eligibility,disclaimer_details,contact_email_for_offers
+        from `tabSupplier`
+        where display_on_partner_listing_page=1 and
+        supplier_type ='Affiliate Partner' order by name""",as_dict=1)
+
+@frappe.whitelist(allow_guest=True)
+def send_email(name,company,email,phone,is_guest,vendor_list):
+    vendor_offer_doc = frappe.get_single('Vendor Carousel and Email template')
+    raw_subject=vendor_offer_doc.subject or ''
+    raw_email=vendor_offer_doc.email or ''
+    if is_guest==True:
+        is_guest='No, it is guest user'
+    else:
+        is_guest='Yes, it is member user'
+    raw_vendor_list=json.loads(vendor_list)
+    count=0
+    for vendor in raw_vendor_list:
+
+        subject=raw_subject.replace('{sender_name}', name or "").replace('{sender_company}',company or "").replace('{sender_email}',email or "").replace('{sender_phone}',phone or "").replace('{sender_is_guest}',is_guest or "").replace('{vendor_name}',vendor or "")
+        email=raw_email.replace('{sender_name}', name or "").replace('{sender_company}',company or "").replace('{sender_email}',email or "").replace('{sender_phone}',phone or "").replace('{sender_is_guest}',is_guest or "").replace('{vendor_name}',vendor or "")
+        vendor_email= frappe.db.sql("""select contact_email_for_offers from  `tabSupplier` where name=%s""",vendor,as_dict=1)
+        outgoing_email_id = frappe.get_doc("Email Account", {"default_outgoing": "1"})
+        if outgoing_email_id:
+            recipients = split_emails(frappe.db.get_value("Supplier", filters={"name": vendor}, fieldname="contact_email_for_offers"))
+            frappe.sendmail(recipients=recipients, message=email, subject=subject)
+            recipients=None
+            subject=None
+            email=None
+            count=count+1
+    return count
+
